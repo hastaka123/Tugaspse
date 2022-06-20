49573a02d257e4854cc17a0f33746bfe799a19df
hastaka123 <107833075+hastaka123@users.noreply.github.com>
2022-06-20 15:36:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style1.css">
+<style>
+a:link, a:visited {
+  background-color: #000;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: #fff;
+}
+</style>
+</head>
+<body>
+<div class="wrapper">
+<center><p><b><font size="17">METODE HITUNG DEPRESIASI</font></b></p></center></br></br></br>
+            <center><a href="straight_line.php"><font size="5" color="red">Straight Line</font></a></br></br>
+            <a href="reducing_balance.php"><font size="5" color="red">Reducing Balance</font></a></br></br>
+            <a href="sum_of_the_year.php"><font size="5" color="red">Sum of The Year</font></a></br></br>
+            <a href="unit_of_activity.php"><font size="5" color="red">Unit of Activity</font></a></br></br></center>
+</div>
+<div id="footer">
+        <p><center>Daniswara Gusti Rafindra & Hastaka Juan Jaya Saputra<br>L200190007 & L200190015</p></center>
+    </div>
+</body>
+</html>
